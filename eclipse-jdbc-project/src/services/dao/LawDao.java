4d20aee422fdb5f1e42753f03fd450d84a491f7c
trainer <trainer@esprit>
2016-05-03 12:03:43 +0100

    add law
    Signed-off-by: trainer <trainer@esprit>

--- eclipse-jdbc-project/src/services/dao/LawDao.java
+++ eclipse-jdbc-project/src/services/dao/LawDao.java
@@ -1,9 +1,30 @@
 package services.dao;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import persistence.Law;
+import utilities.JdbcUtility;
 
 public class LawDao {
+	private Statement statement;
+
 	public void addLaw(Law law) {
+		String sql = "insert into law values(" + law.getId() + ",'" + law.getName() + "') ";
+		try {
+			statement.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Statement getStatement() {
+		try {
+			statement = JdbcUtility.giveMeJdbcConnection().createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return statement;
 	}
 
 }
---
+++ eclipse-jdbc-project/src/tests/TestAddLaw.java
@@ -0,0 +1,20 @@
+package tests;
+
+import persistence.Law;
+import services.dao.LawDao;
+
+public class TestAddLaw {
+	private static LawDao lawDao = new LawDao();
+
+	public static void main(String[] args) {
+		// creation de law
+		Law law = new Law();
+		law.setId(1);
+		law.setName("one");
+
+		// adding the law
+		lawDao.addLaw(law);
+
+	}
+
+}
