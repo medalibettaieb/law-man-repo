acf7040133d1a6cc0d40071a332d8fb293b99bc8
trainer <trainer@esprit>
2016-05-12 12:51:44 +0100

    Signed-off-by: trainer <trainer@esprit>

---
+++ law-man/law-man-client/src/test/java/junit/TestManageLaw.java
@@ -0,0 +1,18 @@
+package junit;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestManageLaw {
+	@Before
+	public void init() {
+
+	}
+
+	@Test
+	public void test() {
+		Assert.assertTrue(1 == 1);
+	}
+
+}
